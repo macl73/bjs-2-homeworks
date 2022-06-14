613af7b6425b5ba50026e6cfb7ff4c2cba5f921d
macl73 <la-paev@yandex.ru>
2022-06-14 18:51:04 +0400

    objects finished

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(mark1, mark2, mark3, ...rest) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark1, mark2, mark3, ...rest);
+  } else {
+    this.marks.push(mark1, mark2, mark3, ...rest);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
